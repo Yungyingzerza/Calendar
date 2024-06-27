8be36716f1deb8877840b368bae66d182ab2d165
Yungyingzerza <yungyingzerza2547@gmail.com>
2024-06-27 20:27:12 +0700

    add navbar

--- Frontend/calendar-frontend/src/App.tsx
+++ Frontend/calendar-frontend/src/App.tsx
@@ -4,11 +4,18 @@ function App() {
   return (
     <>
       <div className="flex flex-col md:flex-row flex-wrap">
+        {/* Mobile */}
+        <div className="w-full md:hidden block">
+          <Navbar />
+        </div>
         <div className="w-full md:w-72 md:h-screen p-2">
           <Sidebar />
         </div>
         <div className="flex-1 flex flex-col h-screen justify-center items-center">
-          <Navbar />
+          {/* PC */}
+          <div className="w-full md:block hidden">
+            <Navbar />
+          </div>
           <div className="w-[calc(100%-75px)] h-[calc(100vh-75px)] rounded-2xl bg-base-200 bg-opacity-20 backdrop-blur-2xl shadow-xl p-2 my-5">
 
           </div>
