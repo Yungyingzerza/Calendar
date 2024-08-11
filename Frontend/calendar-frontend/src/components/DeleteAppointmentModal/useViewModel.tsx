130ecaa74cd1eb529fb0dff44ce873bce82e0e01
Yungyingzerza <yungyingzerza2547@gmail.com>
2024-08-12 01:01:50 +0700

    optimize DeleteAppointmentModal

--- Frontend/calendar-frontend/src/components/DeleteAppointmentModal/useViewModel.tsx
+++ Frontend/calendar-frontend/src/components/DeleteAppointmentModal/useViewModel.tsx
@@ -1,14 +1,15 @@
 import { useSelector } from 'react-redux';
 import { useDeleteAppointmentById } from 'hooks/useDeleteAppointmentById';
+import { useCallback } from 'react';
 export default function useViewModel(){
     const {title, startHour, startMinute, endHour, endMinute, startDay, startMonth, startYear, endDay, endMonth, endYear, id} = useSelector((state: any) => state.draggedItem);
     
     const {deleteAppintment} = useDeleteAppointmentById(id);
 
-    const handleDelete = () => {
+    const handleDelete = useCallback(() => {
         deleteAppintment();
         (document.getElementById('deleteAppointmentModal') as any).close();
-    }
+    }, [deleteAppintment]);
 
     return {
         title,
